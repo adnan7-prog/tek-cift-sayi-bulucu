af66b32bbc06827aadedde4376b52c5b630cfd00
adnan7-prog <getavatar7@gmail.com>
2026-04-25 15:01:45 +0300

    Add files via upload

---
+++ çaılçma.py
@@ -0,0 +1,8 @@
+numper1=int(input("enter a numper : "))
+
+if numper1 <=0 :
+    print("the entered numper cannot be less than zero ")
+elif numper1%2 ==0:
+    print(f"{numper1 }is an even nuper")
+else :
+    print(f"{numper1 } is an odd numper")
